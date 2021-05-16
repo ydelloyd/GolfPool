53cc95ef411b613edec519e9328347564a584bdc
Wendy Cai <wen@MacBook-Pro.attlocal.net>
2021-05-16 07:55:10 -0400

    [ydelloyd] baseendpoint url to https.

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,1 +1,1 @@
-export const baseUrl = window.location.href.includes("localhost") ? "http://localhost:3000/" : "http://golfpool-env-alpha.eba-hamnav8d.us-east-1.elasticbeanstalk.com/";
+export const baseUrl = window.location.href.includes("localhost") ? "http://localhost:3000/" : "https://golfpool-env-alpha.eba-hamnav8d.us-east-1.elasticbeanstalk.com/";
