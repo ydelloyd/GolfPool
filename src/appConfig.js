c6ecdd8f4399af3fd24489950a3150ba8262c00c
Wendy Cai <wen@MacBook-Pro.attlocal.net>
2021-05-15 21:23:34 -0400

    [ydelloyd] updated the backend endpoint.

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,1 +1,1 @@
-export const baseUrl = window.location.href.includes("localhost") ? "http://localhost:3000/" : "https://protected-citadel-17952.herokuapp.com/";
+export const baseUrl = window.location.href.includes("localhost") ? "http://localhost:3000/" : "http://golfpool-env-alpha.eba-hamnav8d.us-east-1.elasticbeanstalk.com/";
